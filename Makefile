aad971c69bab98dcfa33627fcfb23432391664fc
Paul Miller <jettero@gmail.com>
2009-12-15 07:34:07 -0500

    I skelled up this application... the icon is fooby though

---
+++ Makefile
@@ -0,0 +1,20 @@
+name=JSONLocationTracker
+
+default: test
+
+test: build
+	palm-install *.ipk
+	/usr/local/bin/novacom/novacom -t open tty://; echo; echo
+
+myinstall: clean build
+	scp *.ipk castle.vhb:
+	ssh castle.vhb
+
+build:
+	@-rm -vf *.ipk $(name) *.tar.gz ipkgtmp*
+	ln -sf ./ $(name) && \
+        palm-package --exclude "*.tar.gz" --exclude .git --exclude cgi --exclude "*.ipk" \
+                     --exclude $(name) --exclude contrib --exclude Makefile \
+        $(name) && rm $(name)
+clean:
+	git clean -dfx
---
+++ stage-assistant.js
@@ -0,0 +1,21 @@
+function StageAssistant() {
+	Mojo.Log.info("StageAssistant()")
+}
+
+StageAssistant.prototype.setup = function() {
+	Mojo.Log.info("StageAssistant()::setup()")
+    this.controller.assistant.showScene('jlt', 'panel');
+}
+
+StageAssistant.prototype.showScene = function (directory, sceneName, args) {
+	Mojo.Log.info("StageAssistant()::showScene(%s, %s)", directory, sceneName)
+
+	if (args === undefined) {
+		this.controller.pushScene({name: sceneName, sceneTemplate: directory + "/" + sceneName});
+
+	} else {
+		this.controller.pushScene({name: sceneName, sceneTemplate: directory + "/" + sceneName}, args);
+	}
+};
+
+Mojo.Log.info('loaded(stage-assistant.js)');
