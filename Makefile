c4ba5ad447042ba36793259c370f11cfb0729272
Paul Miller <jettero@gmail.com>
2009-12-26 07:29:25 -0500

    I don't really remember what I was working on

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-name=JSONLocationTracker
+name=JetsLocationTracker
 
 default: test
 
--- stage-assistant.js
+++ stage-assistant.js
@@ -4,6 +4,15 @@ function StageAssistant() {
 
 StageAssistant.prototype.setup = function() {
 	Mojo.Log.info("StageAssistant()::setup()")
+
+    // this.appController = Mojo.Controller.getAppController();
+    // this.indexStageController = this.appController.getActiveStageController();
+    //
+    // this.indexStageController.setupWidget("loading",
+    //     this.attributes = { spinnerSize: 'large' },
+    //     this.model      = { spinning: false }
+    // );
+
     this.controller.assistant.showScene('jlt', 'panel');
 }
 
