479f22b6a3b73d49e1de96fb79cefc6b6a7d72d2
Paul Miller <jettero@gmail.com>
2010-01-04 16:08:20 -0500

    much tweaking and shitting myself, slider still doesn't work though

--- app/assistants/ControlPanel.js
+++ app/assistants/ControlPanel.js
@@ -14,40 +14,76 @@ ControlPanelAssistant.prototype.setup = function() {
     });
     */
 
+    var options = {
+        name:    "JLTOptions",
+        version: 1,
+        replace: false // opening existing if possible
+    };
+
+    this.dbo = new Mojo.Depot(options, function(){}, function(t,r){
+        Mojo.Controller.errorDialog("Can't open location database (#" + r.message + ").");
+    });
+
+    Mojo.Log.info("restoring=true (1)");
+    this.restoring = true;
+
     this.trackingOpts = {};
     this.trackingModel = { value: false };
     this.controller.setupWidget('trackingToggle', this.trackingOpts, this.trackingModel);		
-    this.controller.get('trackingToggle').observe(Mojo.Event.propertyChange, this.trackingChanged.bind(this));
+    this.trackingChanged = this.trackingChanged.bindAsEventListener(this);
+    this.controller.get('trackingToggle').observe(Mojo.Event.propertyChange, this.trackingChanged);
 
     this.continuousOpts = {};
     this.continuousModel = { value: true };
     this.controller.setupWidget('continuousUpdates', this.continuousOpts, this.continuousModel);		
-    this.controller.get('continuousUpdates').observe(Mojo.Event.propertyChange, this.continuousChanged.bind(this));
+    this.continuousChanged = this.continuousChanged.bindAsEventListener(this);
+    this.controller.get('continuousUpdates').observe(Mojo.Event.propertyChange, this.continuousChanged);
+    this.continuousChanged();
 
     this.URLAttributes = {
         hintText:      'http://mysite/cgi/path',
         textFieldName: 'postURL',
         maxLength:     2048,
+        textCase:      Mojo.Widget.steModeLowerCase,
+        autoFocus:     false,
+        enterSubmits:  true,
+        holdToEdit:    true, // otherwise it steals focus first thing
         multiline:     false
     };
-    this.URLModel = { value: '' };
+    this.URLModel = { original: '', value: '' };
     this.controller.setupWidget('postURL', this.URLAttributes, this.URLModel);
+    this.URLChanged = this.URLChanged.bind(this);
+    Mojo.Event.listen($('postURL'), Mojo.Event.propertyChange, this.URLChanged);
 
     this.updateIntervalAttributes = {
-        modelProperty: 'value',
-        maxValue: 3600*2,
         minValue: 5,
-        round: true,
-        updateInterval: 0.1
+        maxValue: 7200,
+        updateInterval: 0.1, // this is 100ms I guess, doesn't seem to do anything... who knows
+        round: true
     };
-    this.updateIntervalModel = { value: 1800 };
+    this.updateIntervalModel = { value: 50 };
     this.controller.setupWidget('updateInterval', this.updateIntervalAttributes, this.updateIntervalModel);
-    Mojo.Event.listen($('updateInterval'), 'mojo-property-change', this.updateIntervalChanged.bindAsEventListener(this));
+    this.updateIntervalChanged = this.updateIntervalChanged.bindAsEventListener(this);
+    Mojo.Event.listen($("updateInterval"), Mojo.Event.propertyChange, this.updateIntervalChanged);
     this.updateIntervalChanged();
     $('updateIntervalGroup').hide();
-}
 
-ControlPanelAssistant.prototype.updateIntervalChanged = function() {
+    this.updateIntervalModel.value = 50;
+    this.controller.modelChanged(this.updateIntervalModel);
+
+    this.updateIntervalModel.value = 50;
+    this.controller.modelChanged();
+
+    this.restoring = false;
+};
+
+ControlPanelAssistant.prototype.URLChanged = function() {
+    Mojo.Log.info("ControlPanel::URLChanged(): %s", this.URLModel.value);
+
+    this.savePrefs();
+};
+
+ControlPanelAssistant.prototype.updateIntervalChanged = function(event) {
     var i = parseInt(this.updateIntervalModel.value);
 
     Mojo.Log.info("ControlPanel::updateIntervalChanged(): %d seconds", i);
@@ -66,46 +102,118 @@ ControlPanelAssistant.prototype.updateIntervalChanged = function() {
     }
 
     $('updateIntervalCurrent').innerHTML = s;
-}
 
-ControlPanelAssistant.prototype.trackingChanged = function() {
+    this.savePrefs();
+};
+
+ControlPanelAssistant.prototype.trackingChanged = function(event) {
     Mojo.Log.info("ControlPanel::trackingChanged()", this.trackingModel.value ? "on" : "off");
-}
+};
 
-ControlPanelAssistant.prototype.continuousChanged = function() {
+ControlPanelAssistant.prototype.continuousChanged = function(event) {
     Mojo.Log.info("ControlPanel::continuousChanged(): %s", this.continuousModel.value ? "on" : "off");
 
     if( this.continuousModel.value ) $('updateIntervalGroup').hide();
     else $('updateIntervalGroup').show();
 
-}
+    this.savePrefs();
+};
 
 ControlPanelAssistant.prototype.trackingSuccessResponseHandler = function(result) {
     var asJSON = Object.toJSON(result);
+
     Mojo.Log.info("ControlPanel::trackingSuccessResponseHandler(): %s", asJSON);
-}
+};
 
 ControlPanelAssistant.prototype.trackingFailedResponseHandler = function(result) {
     var errCode = result.errorCode;
     var errStr  = this.errCodeToStr(errCode);
+
     Mojo.Log.info("ControlPanel::trackingFailedResponseHandler() = %s (%d)", errStr, errCode);
-}
+};
 
 ControlPanelAssistant.prototype.activate = function(event) {
     Mojo.Log.info("ControlPanel::activate()");
-}
+
+    this.restorePrefs();
+};
+
+ControlPanelAssistant.prototype.restorePrefs = function() {
+    this.dbo.simpleGet("prefs",
+        function(prefs) {
+            Mojo.Log.info("restoring prefs: %s", Object.toJSON(prefs));
+
+            if( prefs === null )
+                return;
+
+            this.restoring = true;
+
+            this.updateIntervalModel.value = prefs.updateInterval;
+            this.continuousModel.value     = prefs.continuous;
+            this.URLModel.value            = prefs.URL;
+
+            this.updateIntervalModel.value = 50;
+
+            this.controller.modelChanged(this.updateIntervalModel);
+            this.controller.modelChanged(this.continuousModel);
+            this.controller.modelChanged(this.URLModel);
+
+            this.updateIntervalChanged(); // updates some other UI items
+            this.continuousChanged(); // updates some other UI items
+
+            this.restoring = false;
+
+            Mojo.Log.info("restored prefs: %s", Object.toJSON(prefs));
+
+        }.bind(this),
+
+        function(transaction, error) {
+            Mojo.Controller.errorDialog("ERROR restoring prefs (#" + error.message + ").");
+
+        }.bind(this)
+    );
+};
+
+ControlPanelAssistant.prototype.savePrefs = function() {
+    Mojo.Log.info("save() restoring=%s (aborts when true)", this.restoring ? "true" : "false");
+
+    if( this.restoring )
+        return;
+
+    var prefs = {
+        URL:            this.URLModel.value,
+        continuous:     this.continuousModel.value,
+        updateInterval: this.updateIntervalModel.value
+    };
+
+    this.dbo.simpleAdd("prefs", prefs,
+        function() {
+            Mojo.Log.info("saved prefs: %s", Object.toJSON(prefs) );
+
+        }.bind(this),
+
+        function(transaction,result) {
+            Mojo.Controller.errorDialog("ERROR saving prefs (#" + error.message + ").");
+        }.bind(this)
+    );
+};
 
 ControlPanelAssistant.prototype.deactivate = function(event) {
     Mojo.Log.info("ControlPanel::deactivate()");
-}
+
+    this.savePrefs();
+};
 
 ControlPanelAssistant.prototype.cleanup = function(event) {
+    Mojo.Log.info("ControlPanel::cleanup()");
+
     // XXX: What needs to be cleaned up?  Seriously.  Does any of this clean
     // itself up? or do you have to go through and destroy each object and
     // click handler?
 
-    this.trackingHandle.cancel();
-}
+    if( this.trackingHandle )
+        this.trackingHandle.cancel();
+};
 
 ControlPanelAssistant.prototype.errCodeToStr = function(errorCode) {
     var res = {
@@ -120,8 +228,8 @@ ControlPanelAssistant.prototype.errCodeToStr = function(errorCode) {
         8: "The application has been temporarily blacklisted."
     };
 
-    if( res[errorCode] == undefined )
+    if( res[errorCode] === undefined )
         return "something bad happened, error unknown";
 
     return res[errorCode];
-}
+};
