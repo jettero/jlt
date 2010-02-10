d1efa22c25b523402f59ab3de1c0abb13b7396d1
Paul Miller <jettero@gmail.com>
2010-02-10 08:22:36 -0500

    fix recommended by kylegato

--- cgi/perl/gps.sql
+++ cgi/perl/gps.sql
@@ -1,13 +1,14 @@
 CREATE TABLE `gps` (
   `gpsid` bigint(20) unsigned NOT NULL auto_increment,
-  `latitude` double(9,5) unsigned NOT NULL,
-  `longitude` double(9,5) unsigned NOT NULL,
+  `latitude` double(9,5) NOT NULL,
+  `longitude` double(9,5) NOT NULL,
   `haccuracy` int(11) NOT NULL default '-1',
-  `altitude` int(10) unsigned NOT NULL,
+  `altitude` int(11) NOT NULL,
   `vaccuracy` int(11) NOT NULL default '-1',
   `speed` int(10) unsigned NOT NULL,
   `heading` double(6,3) unsigned NOT NULL,
   `readtime` datetime NOT NULL,
-  `updated` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-  PRIMARY KEY  (`gpsid`)
+  `updated` timestamp NOT NULL,
+  PRIMARY KEY  (`gpsid`),
+  UNIQUE KEY `readtime` (`readtime`)
 );
